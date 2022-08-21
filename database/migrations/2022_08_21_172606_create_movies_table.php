af712eaf152dcb0ef8c8611489e8f41a6f8e2d19
Mehmet bozkurt <evilbozkurt@outlook.com>
2022-08-21 21:53:49 +0300

    first update

---
+++ database/migrations/2022_08_21_172606_create_movies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('category_id')->nullable()->constrained()->setNullOnDelete();
+
+            $table->string('slug')->unique();
+
+            $table->string('title');
+            $table->longText('description');
+
+            $table->string('image');
+            $table->string('trailer');
+            $table->string('year');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
